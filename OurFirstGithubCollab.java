12689cbc104a5966e4f8847ff14c6158cfd9f9d3
dasproclass <vit.zuraida@polinema.ac.id>
2023-09-13 10:00:08 +0700

    Revert "mulai fitur kedua"
    This reverts commit 8431028d37e832f7ee91c95e82f1b27e8dd14708.

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -1,8 +1,5 @@
 public class OurFirstGithubCollab{
     public static void main(String[] args){
-        System.out.println("Ini ceritanya fitur pertama sudah berjalan baik");
-
-        System.out.println("Ketua mulai mengembangkan fitur kedua");
-        System.out.println("Tambahan dari anggota 1");
+        System.out.println("Ini ceritanya fitur pertama yang sudah berjalan baik");
     }
 }
